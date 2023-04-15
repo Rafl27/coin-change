b0226897748f52cbf22479c931d928cd7448aa5b
Rafl27 <raflsouza27@gmail.com>
2023-04-15 11:30:52 -0300

    log fix

--- coinChange.js
+++ coinChange.js
@@ -1,6 +1,5 @@
 function coinChange(S, C) {
     let moedas = []
-
     // começamos da maior moeda para ter uma quantidade de moedas menor
     // se começamos da menor s[0] teriamos 147 moedas de 1 😣
     for (let i = S.length - 1; i >= 0; i--) {
@@ -8,7 +7,7 @@ function coinChange(S, C) {
         while (C >= S[i]) {
             // ela é adicionada ao array de moedas
             moedas.push(S[i])
-            // subtraimos do troco a moeda adicionada
+            // subtraimos do troco a moeda adicionada, assim sabemos quanto ainda falta
             C -= S[i]
         }
     }
@@ -17,7 +16,6 @@ function coinChange(S, C) {
 
 const S = [1, 5, 10, 25, 50, 100]
 const C = 147
-
 const Troco = coinChange(S, C)
 
-console.log(`[${Troco}]\nSendo necessário ${Troco.length} moedas.`)
+console.log(`Sistema de moedas: [${S}]\nTroco: ${C}\nMoedas usadas: [${Troco}]\nSendo ${Troco.length} a menor quantidade possível de moedas.`)
