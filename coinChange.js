883baf373949307a8517dab20cc7ac93caf8cbd3
Rafl27 <raflsouza27@gmail.com>
2023-04-15 11:17:01 -0300

    first commit

---
+++ coinChange.js
@@ -0,0 +1,23 @@
+function coinChange(S, C) {
+    let moedas = []
+
+    // começamos da maior moeda para ter uma quantidade de moedas menor
+    // se começamos da menor s[0] teriamos 147 moedas de 1 😣
+    for (let i = S.length - 1; i >= 0; i--) {
+        //enquanto essa moeda for >= ao troco C
+        while (C >= S[i]) {
+            // ela é adicionada ao array de moedas
+            moedas.push(S[i])
+            // subtraimos do troco a moeda adicionada
+            C -= S[i]
+        }
+    }
+    return moedas
+}
+
+const S = [1, 5, 10, 25, 50, 100]
+const C = 147
+
+const Troco = coinChange(S, C)
+
+console.log(`[${Troco}]\nSendo necessário ${Troco.length} moedas.`)
